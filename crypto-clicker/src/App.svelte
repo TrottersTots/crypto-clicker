8e2ed87075101ea777e2a56ef6bdc04d8f380308
Nathan Inbar <nathaninbar@gmail.com>
2022-02-25 16:43:36 -0800

    added html elements for flex structure

--- crypto-clicker/src/App.svelte
+++ crypto-clicker/src/App.svelte
@@ -1,30 +1,40 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="content">
+    <div class="info__container">
+      <h1>info container</h1>
+    </div>
+    <div class="main__container">
+      <h1>main container</h1>
+    </div>
+    <div class="upgrade__container">
+      <h1>upgrade container</h1>
+    </div>
+  </div>
+  <!-- end content -->
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
