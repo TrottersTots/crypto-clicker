0ef73fb1fa76fd269e522c119108c77e6a0c7682
Nathan Inbar <nathaninbar@gmail.com>
2022-02-25 18:59:19 -0800

    changed bkg repeat

--- crypto-clicker/src/Components/Container.svelte
+++ crypto-clicker/src/Components/Container.svelte
@@ -12,8 +12,9 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: moccasin;
-    border: 1px solid brown;
+    background-color: rgba(54, 212, 191, 0.8);
     margin: var(--global_margin);
+  }
+  div > h1 {
   }
 </style>
