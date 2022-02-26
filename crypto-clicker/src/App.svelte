8c772783f0e927c21338c76a30de78c2e7424902
Justin Stitt <jkstitt@csu.fullerton.edu>
2022-02-25 18:11:31 -0800

    🌿 merge upgrades and flex

--- crypto-clicker/src/App.svelte
+++ crypto-clicker/src/App.svelte
@@ -1,18 +1,6 @@
 <script>
   import Container from "./Components/Container.svelte";
 
-  export let name;
-</script>
-
-<div class="title">
-  <h1>Crypto Clicker</h1>
-  <h4>Nathan Inbar && Justin Stitt</h4>
-</div>
-<div class="content">
-  <Container title="info" grow={1} />
-  <Container title="main" grow={2} />
-  <Container title="upgrade" grow={1} />
-</div>
 	import Cookie from "./Components/Cookie.svelte";
 	import Clicker from "./Components/Clicker.svelte";
 	import Upgrade from "./Components/Upgrade.svelte";
@@ -28,18 +16,32 @@
 
 </script>
 
-<main>
-	<h1>Crypto Clicker</h1>
-	<h2>{$score.toFixed(5)} : BTC</h2>
-	<Cookie bind:this={cookie}/>
-	<Upgrade target={cookie}/>
-	<button on:click={addClicker}>
-		Add Clicker
-	</button>
-	{#each clickers as clicker}
-		<Clicker/>
-	{/each}
-</main>
+
+<div class="title">
+  <h1>Crypto Clicker</h1>
+</div>
+
+<div class="content">
+  <Container title="info" grow={1} />
+  <Container title="main" grow={2}>
+    <Cookie bind:this={cookie}/>
+	  <h2>{$score.toFixed(5)} : BTC</h2>
+	
+    <button on:click={addClicker}>
+      Add Clicker
+    </button>
+    {#each clickers as clicker}
+      <Clicker/>
+    {/each}
+  </Container>
+  <Container title="upgrade" grow={1}>
+    <Upgrade target={cookie}/>
+  </Container>
+</div>
+
+<div class='footer'>
+  <h4>Nathan Inbar && Justin Stitt</h4>
+</div>
 
 <!-- end content -->
 <style>
--- crypto-clicker/src/Components/Container.svelte
+++ crypto-clicker/src/Components/Container.svelte
@@ -5,11 +5,13 @@
 
 <div style={`flex-grow: ${grow}`}>
   <h1>{title} container</h1>
+  <slot></slot>
 </div>
 
 <style>
   div {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     background-color: moccasin;
